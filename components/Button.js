4fcecc15effda63e70725769e9933aa3d6597bd9
Emmanuel-Youpy <emmydeji58@gmail.com>
2022-12-03 11:52:02 +0800

    first commit

---
+++ components/Button.js
@@ -0,0 +1,76 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Touchable,
+  Image,
+  Alert,
+} from "react-native";
+import { COLORS, SIZES, SHADOWS, FONTS } from "../constants";
+import { useNavigation } from "@react-navigation/native";
+
+const createTwoButtonAlert = () =>
+  Alert.alert("Alert Title", "My Alert Msg", [
+    {
+      text: "Cancel",
+      onPress: () => console.log("Cancel Pressed"),
+      style: "cancel",
+    },
+    { text: "OK", onPress: () => console.log("OK Pressed") },
+  ]);
+export const CircleButton = ({ navigation, imgUrl, handlePress, ...props }) => {
+  return (
+    <TouchableOpacity
+      style={{
+        width: 40,
+        height: 40,
+        backgroundColor: COLORS.white,
+        position: "absolute",
+        borderRadius: SIZES.extraLarge,
+        alignItems: "center",
+        justifyContent: "center",
+        ...SHADOWS.light,
+        ...props,
+      }}
+      onPress={handlePress}
+    >
+      <Image
+        source={imgUrl}
+        resizeMode="contain"
+        style={{ width: 24, height: 24 }}
+      />
+    </TouchableOpacity>
+  );
+};
+
+export const RectButton = ({ minWidth, fontSize, handlePress, ...props }) => {
+  // const navigation = useNavigation();
+  return (
+    <TouchableOpacity
+      style={{
+        backgroundColor: COLORS.primary,
+        padding: SIZES.small,
+        borderRadius: SIZES.extraLarge,
+        minWidth: minWidth,
+        ...props,
+      }}
+      onPress={handlePress}
+      // onPress={() => navigation.navigate("Details")}
+    >
+      <Text
+        style={{
+          fontFamily: FONTS.semiBold,
+          fontSize: fontSize,
+          color: COLORS.white,
+          textAlign: "center",
+        }}
+      >
+        Place a bid
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({});
